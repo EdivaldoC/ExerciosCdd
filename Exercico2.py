f2ebcf109115a0484243e166b00d834f23f4261f
Edivaldo Coelho <113144617+EdivaldoC@users.noreply.github.com>
2024-09-03 13:42:39 -0300

    primiero
    apenas atividades do Cdd

---
+++ Exercico2.py
@@ -0,0 +1,8 @@
+
+v1 = float(input("Digite a primeira nota: "))
+v2 = float(input("Digite a segudna nota: "))
+
+media = (v2+v1)/2
+
+print("================================")
+print("a media do aluno e: ",media)
---
+++ main.py
@@ -0,0 +1,4 @@
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+print("=================================")
+print(f"Ola seu {nome} voce tem {idade}")
