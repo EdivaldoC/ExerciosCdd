be2889cd836011064fad4ea50ec62b35e05b5d4b
Edivaldo Coelho <113144617+EdivaldoC@users.noreply.github.com>
2024-09-05 13:26:11 -0300

    aa

--- Exercicio01.py
+++
@@ -1,9 +0,0 @@
-nomes = str(input("digite o 1 nome"))
-nomes2 = str(input("digite o 2 nome"))
-nomes3 = str(input("digite o 3 nome"))
-
-print(f"Ola {nomes}, {nomes2},{nomes3}")
-print("===================================")
-print(f"Ola {nomes}")
-print(f"Ola {nomes2}")
-print(f"Ola {nomes3}")
--- exercico5.py
+++
@@ -1,6 +0,0 @@
-a = 10
-b = 5
-temp = a
-a = b
-b = temp
-print(a,b)
